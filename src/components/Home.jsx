7a3d85791ba37bd44cf45cf7d9d2df358feaa3e2
eddymandran <eddy.mandran@gmail.com>
2022-01-07 11:13:53 +0100

    display product in admin component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,26 @@
-import {useEffect, useState} from "react";
-import Card from './Card';
+import { useEffect, useState } from "react";
+import Card from "./Card";
 
 const Home = () => {
-    const [products, setProducts] =useState();
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:5050/product")
+      .then((response) => response.json())
+      .then((products) => setProducts(products))
+      .catch((error) => console.error(error));
+  }, []);
 
-    useEffect(() => {
-        fetch('http://localhost:5050/product')
-            .then(response => response.json())
-            .then(products => setProducts(products))
-            .catch(error => console.error(error));
-    }, [])
-
-    console.log((products))
-
+  return products.map((product, index) => {
     return (
-        products.map((product, index) => {
-            return (
-                <Card key={index}
-                      title={product.name}
-                      price={product.price}
-                      stock={product.stock}
-                />
-            )
-        })
-    )
-}
+      <Card
+        key={index}
+        title={product.name}
+        price={product.price}
+        stock={product.stock}
+      />
+    );
+  });
+};
 
 export default Home;
--- src/components/admin/ProductList.jsx
+++ src/components/admin/ProductList.jsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
+
+
 const ProductList = () => {
-    return (
-        <div>ProductList component</div>
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5050/product")
+          .then((response) => response.json())
+          .then((products) => setProducts(products))
+          .catch((error) => console.error(error));
+      }, []);
+
+      return (
+        <>
+            <hr />
+            <table className="table">
+                <thead className="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Stock</th>
+                    <th scope="col">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {products.map(product => {
+                    return (
+                        <tr key={product.id}>
+                            <th scope="row">{product.id}</th>
+                            <td>{product.name}</td>
+                            <td>{product.price} €</td>
+                            <td>{product.stock}</td>
+                           <td> <Link className="btn btn-success" to={`/admin/product/${product.id}/edit`}>Edit</Link></td>
+                        </tr>
+                    );
+                })}
+                </tbody>
+            </table>
+        </>
     )
 }
 
